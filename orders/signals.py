afa6a050a8ed974b21c781a70dbbf564ed6099f9
aryan017 <aryankumar2835@gmail.com>
2021-08-07 20:23:46 +0530

    create signal for orders and connect it with cars

---
+++ orders/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import m2m_changed # many to many
+from django.dispatch import receiver
+from .models import Order
+
+@receiver(m2m_changed,sender=Order.cars.through)
+def m2m_changed_cars_order(sender,instance,action,**kwargs):
+  
+    total=0
+    total_price=0
+    if action=="post_add" or action=="post_remove":
+        print("running")
+        print(action)
+        for car in instance.cars.all():
+             total+=1
+             total_price+=car.price
+        instance.total=total
+        instance.total_price=total_price
+        instance.save()
